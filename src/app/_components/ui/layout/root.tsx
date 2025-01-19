ac37fda89eccbd52734a7be174976486933b6d6a
Nemixe <rafel.ami@gmail.com>
2025-01-20 06:36:21 +0700

    chore: remove unused variable and import in root layout

--- src/app/_components/ui/layout/root.tsx
+++ src/app/_components/ui/layout/root.tsx
@@ -1,14 +1,8 @@
 import type { FC, ReactElement } from "react";
 import { Outlet } from "react-router-dom";
-import { UserCookies } from "@/libs/cookies";
 import { Toaster } from "react-hot-toast";
 
 export const RootLayout: FC = (): ReactElement => {
-  const userData = UserCookies.get();
-  const userPermissions = userData?.role?.permissions?.map((perm) => perm.name) || [];
-
-  console.log(userPermissions);
-
   return (
     <>
       <Outlet />
