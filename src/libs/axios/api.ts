55f974992d7dd9c3d2dfdd3d7c3caccfd29ea4a9
Maulana Sodiqin <sodiqincahyana1@gmail.com>
2025-01-29 13:14:01 +0700

    fix: refresh token

--- src/libs/axios/api.ts
+++ src/libs/axios/api.ts
@@ -30,7 +30,11 @@ api.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config;
 
-    if (error.response.status === 401 && !originalRequest._retry) {
+    if (
+      error.response.status === 401 &&
+      error.response?.data?.message?.toLowerCase()?.includes?.("invalid") &&
+      !originalRequest._retry
+    ) {
       if (isRefreshing) {
         return new Promise<string>((resolve, reject) => {
           failedQueue.push({ resolve, reject });
